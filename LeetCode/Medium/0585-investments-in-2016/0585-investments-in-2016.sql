b24438fb7d930a656d8867ed1c9e83f3cd562a79
Piyush <piyushrajan2007@gmail.com>
2026-05-25 12:11:37 +0530

    Time: 561 ms (69.02%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Medium/0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+SELECT 
+    ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM 
+    Insurance
+WHERE 
+    -- Condition 1: tiv_2015 is not unique
+    tiv_2015 IN (
+        SELECT tiv_2015 
+        FROM Insurance 
+        GROUP BY tiv_2015 
+        HAVING COUNT(*) > 1
+    )
+    -- Condition 2: The (lat, lon) pair is completely unique
+    AND (lat, lon) IN (
+        SELECT lat, lon 
+        FROM Insurance 
+        GROUP BY lat, lon 
+        HAVING COUNT(*) = 1
+    );
