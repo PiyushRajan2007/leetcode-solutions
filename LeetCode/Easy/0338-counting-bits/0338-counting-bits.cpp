84ea74aa39b0fc5da8d8c0ae523b74d6bc80b4c8
Piyush <piyushrajan2007@gmail.com>
2026-07-02 14:53:20 +0530

    Time: 0 ms (100%), Space: 10.4 MB (97.3%) - LeetHub

--- LeetCode/Easy/0338-counting-bits/0338-counting-bits.cpp
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1,0);
+        vector<int> ans(n + 1);
 
         for (int i = 1; i <= n; i++) {
             ans[i] = ans[i & (i - 1)] + 1;
