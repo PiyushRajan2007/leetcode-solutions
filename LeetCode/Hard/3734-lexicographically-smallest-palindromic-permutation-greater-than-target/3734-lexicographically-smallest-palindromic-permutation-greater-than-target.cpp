07600ca8c61a3a2e8e80ef4d4b7c18025950547b
Piyush <piyushrajan2007@gmail.com>
2026-08-28 11:58:43 +0530

    Time: 2 ms (76.92%), Space: 13.7 MB (43.08%) - LeetHub

---
+++ LeetCode/Hard/3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    string lexPalindromicPermutation(string s, string target) {
+        int n = s.size(), m = n / 2, mid = -1;
+        vector<int> c(26);
+
+        for (char x : s) c[x - 'a']++;
+
+        for (int i = 0; i < 26; i++)
+            if (c[i] & 1) {
+                if (mid != -1) return "";
+                mid = i;
+            }
+
+        vector<int> r(26);
+        for (int i = 0; i < 26; i++) r[i] = c[i] / 2;
+
+        string t = target.substr(0, m);
+
+        // Check if target's first half can be used exactly
+        vector<int> cnt = r;
+        bool ok = true;
+
+        for (char x : t)
+            if (--cnt[x - 'a'] < 0) {
+                ok = false;
+                break;
+            }
+
+        auto build = [&](string h) {
+            string ans = h;
+            if (n & 1) ans += char('a' + mid);
+            ans += string(h.rbegin(), h.rend());
+            return ans;
+        };
+
+        if (ok) {
+            string ans = build(t);
+            if (ans > target) return ans;
+        }
+
+        // Find smallest half strictly greater than t
+        for (int i = m - 1; i >= 0; i--) {
+            cnt = r;
+
+            for (int j = 0; j < i; j++)
+                if (--cnt[t[j] - 'a'] < 0) {
+                    cnt.clear();
+                    break;
+                }
+
+            if (cnt.empty()) continue;
+
+            for (int ch = t[i] - 'a' + 1; ch < 26; ch++) {
+                if (!cnt[ch]) continue;
+
+                string h = t.substr(0, i) + char('a' + ch);
+                cnt[ch]--;
+
+                for (int j = 0; j < 26; j++)
+                    h += string(cnt[j], 'a' + j);
+
+                return build(h);
+            }
+        }
+
+        return "";
+    }
+};
