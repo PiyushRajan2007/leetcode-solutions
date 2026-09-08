f68ff36dec22255c861b733b32e542a18b1a3117
Piyush <piyushrajan2007@gmail.com>
2026-09-08 11:11:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

--- LeetCode/Easy/3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ LeetCode/Easy/3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int countCommas(int n) {
-       if(n<1000)
-       return 0;
-       else if(n==1000)
-        return 1;
-        else 
-        return n - 1000 + 1; 
+    
+        return max(n - 999 , 0); 
          
     }
 };
